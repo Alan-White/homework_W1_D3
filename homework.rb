8289a3a60d12533ea34cf3cfab82dede2c64a615
Alan White <alanwhitey@gmail.com>
2017-05-24 17:17:42 +0100

    all questions answered

--- homework.rb
+++ homework.rb
@@ -9,33 +9,44 @@ lines = ['Gyle Centre', 'Edinburgh Park', 'Murrayfield Stadium', 'Haymarket', 'P
 puts lines[1]
 
 # 3. How many ways can we return 'Princes Street' from the array?
-puts lines[4]
+ lines[4] lines[-1]
 
 # 4. Work out the index position of 'Haymarket'
+lines[3]
 
 # 5. Add 'Airport' to the start of the array
+lines[0] = "Airport"
 
 # 6. Add 'York Place' to the end of the array
+lines[5] = "York Place" #assuming Airport hasn't actually been added.
 
 # 7. Remove 'Edinburgh Park' from the array using it's name
+lines.delete("Edinburgh Park")
 
 # 8. Delete 'Edinburgh Park' from the array by index
+lines.delete_at(1)
 
 # 9. Reverse the positions of the stops in the array
+lines.reverse!
 
 ### B. Given the following data structure:
 
 my_hash = {"0" => "Zero", 1 => "One", :two => "Two", "two" => 2}
 
 # 1. How would you return the string `"One"`?
+my_hash [1]
 
 # 2. How would you return the string `"Two"`?
+my_hash [:two]
 
 # 3. How would you return the number `2`?
+my_hash ["two"]
 
 # 4. How would you add `{3 => "Three"}` to the hash?
+my_hash [3] = "Three"
 
 # 5. How would you add `{:four => 4}` to the hash?
+my_hash [:four] = 4
 
 ### C. Given the following data structure:
 
@@ -72,19 +83,28 @@ users = {
 }
 
 # 1. Return Jonathan's Twitter handle (i.e. the string `"jonnyt"`)
+users ["Jonathan"][:twitter]
 
 # 2. Return Erik's hometown
+users ["Erik"][:home_town]
 
 # 3. Return the array of Erik's favorite numbers
+users ["Erik"][:favourite_numbers]
 
 # 4. Return the type of Avril's pet Colin
+users ["Avril"][:pets]["colin"]
 
 # 5. Return the smallest of Erik's favorite numbers
+users ["Erik"][:favourite_numbers][0]
 
 # 6. Add the number `7` to Erik's favorite numbers
+users ["Erik"][:favourite_numbers].push(7)
 
 # 7. Change Erik's hometown to Edinburgh
+users ["Erik"][:home_town]="Glenrothes"
 
 # 8. Add a pet dog to Erik called "Fluffy"
+users ["Erik"][:pets]["Fluffy"] = :dog
 
 # 9. Add yourself to the users hash
+users["Alan"] = {}
